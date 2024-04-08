9885c91e7e9740c7bdf1f0f8449193abbb9462da
Ashan Dimantha <ashandimanthalk@gmail.com>
2024-04-09 00:40:13 +0530

    added: home

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+# Main function to run Streamlit app
+def main():
+    st.title('Hey AIESEC in Sri Lanka 💙')
+
+    st.write('Welcome to the AIESEC in Sri Lanka Local Entity Global Rankings. Here, you can explore and compare the performance of Local Committees (LCs) of AIESEC in Sri Lanka.')
+
+    st.title('Features')
+    st.success('🌍 View Global Rankings: Select the function and status to see how different LCs in Sri Lanka are performing globally.')
+    st.success('📊 Entity Performance: Analyze the performance of products and local entities of AIESEC in Sri Lanka.')
+
+    st.title('Limitations')
+    st.warning("⏳ Retrieving Data: Currently, only LCs (Local Committees) of AIESEC in Sri Lanka are accessible. Including global entities in the ranking requires more time to retrieve data from the Expa API.")
+    st.warning("⌛ Data Retrieval Time: The data is retrieved from the Expa API, which may take some time. Please be patient while the data is being fetched.")
+
+    st.markdown('---')
+    st.subheader('About the Developer')
+    # st.image('https://avatars.githubusercontent.com/u/85834813?v=4', width=200)
+    st.write('Hello! I am Ashan Dimantha Herath, MCVP Applicant 24.25 at AIESEC in Sri Lanka.')
+    st.write('If you have any questions or feedback about this application, please reach out to me:')
+    st.write(' Website: https://ashandimantha.vercel.app/')
+    st.write(' Email: ashan.herath16@aiesec.net')
+    st.markdown('---')
+if __name__ == "__main__":
+    main()
